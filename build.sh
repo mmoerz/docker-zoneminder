78013c825d0ab52c89e8ec351c5e97244dddcc57
mmoerz <michael@moerz.co.at>
2020-07-25 22:39:09 +0200

    + moving the build docker image into a subdir

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 version=$(git rev-parse --short HEAD)
 build_date=$(date -Iseconds)
-tagdate=$(echo $build_date | sed 's/-/_/g' | sed 's/:/_/g')
+#tagdate=$(echo $build_date | sed 's/-/_/g' | sed 's/:/_/g')
+tagdate=$(date +%Y_%m_%d__%H%M)
+gituser=$(git config --global user.name)
 
-docker build --no-cache --build-arg "version=$version" --build-arg "build_date=$build_date" -t jantman/zoneminder:${tagdate} .
-docker tag jantman/zoneminder:${tagdate} jantman/zoneminder:latest
+docker build --no-cache --build-arg "version=$version" --build-arg "build_date=$build_date" -t $gituser/zoneminder:${tagdate} .
+docker tag $gituser/zoneminder:${tagdate} $gituser/zoneminder:latest
