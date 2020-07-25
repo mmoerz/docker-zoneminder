abe888274721549e0e077383dedcc4e545bd9d87
mmoerz <michael@moerz.co.at>
2020-07-25 22:10:50 +0200

    adding abuild stuff for building packages

--- build.sh
+++ build.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+NOCACHE=--no-cache
+NOCACHE=
+
 version=$(git rev-parse --short HEAD)
 build_date=$(date -Iseconds)
 #tagdate=$(echo $build_date | sed 's/-/_/g' | sed 's/:/_/g')
 tagdate=$(date +%Y_%m_%d__%H%M)
 gituser=$(git config --global user.name)
 
-docker build --no-cache --build-arg "version=$version" \
+docker build $NOCACHE --build-arg "version=$version" \
   --build-arg "build_date=$build_date" -t $gituser/zoneminder:${tagdate} build
 docker tag $gituser/zoneminder:${tagdate} $gituser/zoneminder:latest
 
