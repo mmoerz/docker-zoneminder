a4f1ba4cf99b8b29f08dd8b5b52d60c1c0457cd7
mmoerz <michael@moerz.co.at>
2020-07-26 04:26:55 +0200

    should at last build zoneminder

--- build/buildall.sh
+++ build/buildall.sh
@@ -4,11 +4,12 @@ ORDER_TO_BUILD="perl-class-std
 perl-class-std-fast
 perl-io-interface
 perl-io-socket-multicast
-perl-test-xml
 perl-test-pod-content
-perl-xml-sax-writer
-perl-xml-filter-buffertext
 perl-xml-semanticdiff
+perl-xml-filter-buffertext
+perl-xml-sax-writer
+perl-test-xml
+perl-soap-wsdl
 "
 
 for DIR in $ORDER_TO_BUILD
