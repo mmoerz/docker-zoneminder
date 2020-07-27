67201b553e06490ef68a6fcaa51d7ee54693441c
mmoerz <michael@moerz.co.at>
2020-07-27 20:41:55 +0200

    fixed build of zm-base

--- zm-base/ressources/install.sh
+++ zm-base/ressources/install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env ash
 
-install -m 0644 -o root -g root /tmp/build/apache2_zoneminder.conf /etc/apache2/conf.d/zoneminder.conf 
-install -m 0644 -o root -g root /tmp/build/supervisord.conf /etc/supervisord.conf 
-install -m 0755 -o root -g root /tmp/build/mysql.sh /usr/bin/zm_mysql 
-install -m 0644 -o root -g root /tmp/build/php-fpm.conf /etc/php7/php-fpm.conf 
-install -m 0644 -o root -g root /tmp/build/zm.conf /etc/zm.conf 
-install -m 0644 -o root -g root /tmp/build/amcrest841.pm /usr/share/perl5/vendor_perl/ZoneMinder/Control/amcrest841.pm 
-mkdir -p /var/lib/zoneminder /var/run/zoneminder 
-rm -Rf /tmp/build
+res=/root/build
 
+install -m 0644 -o root -g root $res/apache2_zoneminder.conf /etc/apache2/conf.d/zoneminder.conf 
+install -m 0644 -o root -g root $res/supervisord.conf /etc/supervisord.conf 
+install -m 0755 -o root -g root $res/mysql.sh /usr/bin/zm_mysql 
+install -m 0644 -o root -g root $res/php-fpm.conf /etc/php7/php-fpm.conf 
+install -m 0644 -o root -g root $res/zm.conf /etc/zm.conf 
+#install -m 0644 -o root -g root /tmp/build/amcrest841.pm /usr/share/perl5/vendor_perl/ZoneMinder/Control/amcrest841.pm 
+mkdir -p /var/lib/zoneminder /var/run/zoneminder 
+rm -rf $res
