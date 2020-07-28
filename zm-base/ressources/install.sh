403e1940e0e11366a3495f82be92c8aa04d099ec
mmoerz <michael@moerz.co.at>
2020-07-28 17:41:50 +0200

    finally a working version

--- zm-base/ressources/install.sh
+++ zm-base/ressources/install.sh
@@ -2,6 +2,8 @@
 
 res=/root/build
 
+install -m 0644 -o root -g root $res/php-timezone.ini /etc/php7/conf.d/timezone.ini
+
 install -m 0644 -o root -g root $res/apache2_zoneminder.conf /etc/apache2/conf.d/zoneminder.conf 
 install -m 0644 -o root -g root $res/supervisord.conf /etc/supervisord.conf 
 install -m 0755 -o root -g root $res/mysql.sh /usr/bin/zm_mysql 
