c6bb61671e46a31bf2b5a0d7adedcf3f49bffc62
mmoerz <michael@moerz.co.at>
2020-07-29 00:06:00 +0200

    fixed several problems related to missing directories

--- zm-base/ressources/install.sh
+++ zm-base/ressources/install.sh
@@ -10,5 +10,13 @@ install -m 0755 -o root -g root $res/mysql.sh /usr/bin/zm_mysql
 install -m 0644 -o root -g root $res/php-fpm.conf /etc/php7/php-fpm.conf 
 install -m 0644 -o root -g root $res/zm.conf /etc/zm.conf 
 #install -m 0644 -o root -g root /tmp/build/amcrest841.pm /usr/share/perl5/vendor_perl/ZoneMinder/Control/amcrest841.pm 
-mkdir -p /var/lib/zoneminder /var/run/zoneminder 
 rm -rf $res
+
+# fixes that should be unecessary once a correcte zoneminder.apk is used>
+mkdir -p /var/lib/zoneminder/events
+mkdir -p /var/lib/zoneminder/images
+mkdir -p /var/run/zoneminder
+mkdir -p /var/cache/zoneminder
+chown apache:www-data /var/lib/zoneminder/events
+chown apache:www-data /var/lib/zoneminder/images
+chown apache:www-data /var/cache/zoneminder
