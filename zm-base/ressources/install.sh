2b23735056b6b467809eddd11391a87389c01376
mmoerz <michael@moerz.co.at>
2020-08-30 19:09:50 +0200

    trying to fix all the problems

--- zm-base/ressources/install.sh
+++ zm-base/ressources/install.sh
@@ -8,7 +8,8 @@ res=/root/build
 install -m 0644 -o root -g root $res/supervisord.conf /etc/supervisord.conf 
 install -m 0755 -o root -g root $res/mysql.sh /usr/bin/zm_mysql 
 #install -m 0644 -o root -g root $res/php-fpm.conf /etc/php7/php-fpm.conf 
-install -m 0644 -o root -g root $res/zm.conf /etc/zm.conf 
+install -m 0644 -o root -g root $res/zm.conf /etc/zoneminder/zm.conf 
+install -m 0644 -o root -g root -d $res/zoneminder/conf.d
 #install -m 0644 -o root -g root /tmp/build/amcrest841.pm /usr/share/perl5/vendor_perl/ZoneMinder/Control/amcrest841.pm 
 rm -rf $res
 
